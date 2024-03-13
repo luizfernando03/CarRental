45357f24114ba2ddb261197faba9dedab303ec0b
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-13 13:31:28 -0300

    Adicionando classes

---
+++ src/main/java/org/ada/locadora/Locadora.java
@@ -0,0 +1,13 @@
+package org.ada.locadora;
+
+import java.util.List;
+
+public class Locadora {
+
+    private List<Veiculo> veiculos;
+    private List<Aluguel> alugueis;
+    private List<Cliente> clientes;
+
+
+
+}
