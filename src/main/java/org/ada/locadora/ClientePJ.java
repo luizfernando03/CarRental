5db0c98db71c3cf8377499c2cf5731ecd1c03d89
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-14 15:26:42 -0300

    Subindo dados da classe ClientePJ

--- src/main/java/org/ada/locadora/ClientePJ.java
+++ src/main/java/org/ada/locadora/ClientePJ.java
@@ -1,4 +1,43 @@
 package org.ada.locadora;
 
-public class ClientePJ {
+public class ClientePJ implements Cliente{
+    private String cnpj;
+    private String nome;
+    private String tipo;
+
+    public ClientePJ(String cnpj, String nome, String tipo) {
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String getIdentificador() {
+        return cnpj;
+    }
+
+    @Override
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
