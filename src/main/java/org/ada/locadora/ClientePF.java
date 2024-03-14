7ee8b0453b1400b2351ff4279556b6104e8b02ec
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-03-14 15:31:16 -0300

    Subindo dados da classe ClientePF

--- src/main/java/org/ada/locadora/ClientePF.java
+++ src/main/java/org/ada/locadora/ClientePF.java
@@ -1,4 +1,28 @@
 package org.ada.locadora;
 
-public class ClientePF {
+public class ClientePF implements Cliente{
+
+    private String cpf;
+    private String nome;
+    private String tipo;
+
+    public ClientePF(String cpf, String nome, String tipo) {
+        this.cpf = cpf;
+        this.nome = nome;
+        this.tipo = "PF";
+    }
+
+    @Override
+    public String getIdentificador() {
+        return null;
+    }
+
+    @Override
+    public String getTipo() {
+        return null;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
